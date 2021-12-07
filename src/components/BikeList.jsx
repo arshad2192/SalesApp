f9e36497732b28065eda313f4cce7c0716126e54
arshad2192 <arshad2192@gmail.com>
2021-12-07 12:26:40 +0530

    commemt

--- src/components/BikeList.jsx
+++ src/components/BikeList.jsx
@@ -1,16 +1,35 @@
-import React, { useState } from "react";
+// <<<<<<< Updated upstream
+// import React, { useState } from "react";
+// import { BikeItem } from "./BikeItem";
+// import { bikes } from "../services/bikes";
+// import { Row, Col, Button } from "react-bootstrap";
+
+// export const BikeList = () => {
+//   const [filtBike, setFiltBike] = useState(bikes);
+//   const [txtSearch, setTxtSearch] = useState("KTM 200 Duke");
+//   const handleSearch = () => {
+//     const filteredBikes = bikes.filter((item) => item.name === txtSearch);
+//     txtSearch !== "" ? setFiltBike(filteredBikes) : setFiltBike(bikes);
+//   };
+// =======
+import React, { useState} from "react";
 import { BikeItem } from "./BikeItem";
 import { bikes } from "../services/bikes";
 import { Row, Col, Button } from "react-bootstrap";
-
+import { width } from "dom-helpers";
 export const BikeList = () => {
   const [filtBike, setFiltBike] = useState(bikes);
-  const [txtSearch, setTxtSearch] = useState("KTM 200 Duke");
+  const [txtSearch, setTxtSearch]= useState("KTM 200 Duke");
   const handleSearch = () => {
-    const filteredBikes = bikes.filter((item) => item.name === txtSearch);
-    txtSearch !== "" ? setFiltBike(filteredBikes) : setFiltBike(bikes);
-  };
-  return (
+   const filteredBikes = bikes.filter(item=> item.name=== txtSearch);
+   if(txtSearch!=="") setFiltBike(filteredBikes);
+   else setFiltBike(bikes)
+  //  or optimum code of if else is:
+  txtSearch !=="" ? setFiltBike(filteredBikes): setFiltBike(bikes);
+
+  }
+// >>>>>>> Stashed changes
+  return(
     <div>
       <Row>
         <Col lg={8}>
@@ -23,6 +42,7 @@ export const BikeList = () => {
         </Col>
         <Col lg={2}>
           <Button onClick={handleSearch}>Search</Button>
+          <Button onClick={() => handleSearch()} >Search</Button>
         </Col>
       </Row>
       <Row>
@@ -34,4 +54,3 @@ export const BikeList = () => {
       </Row>
     </div>
   );
-};
